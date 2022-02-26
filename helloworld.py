b45e4536fb7dfc479bc35b4b412120ff347bc398
vignesh <your_email_address@example.com>
2022-02-26 13:07:54 +0530

    feb 27

---
+++ helloworld.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+print ("Hello World")
+
+
+a = 10
+
+b = 20
+
+c = 30
+
+name = "vignesh"
+
+print (a)
+
+print (b)
+
+print (c)
+
+print (name)
+
+a = 12
+
+b = 15
+
+a + b
+print (a + b)
+
+
+number = int (input("Enter an integer: "))
+if number < 100:
+    print("Your number is smaller than 100")
+else:
+    print("Your number is greater than 100" )   
+    
+       
+       
